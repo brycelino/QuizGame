e9e23e3984df323922c557fa47802fa6aae4d839
brycelino <brycejeff16@gmail.com>
2022-04-29 03:17:29 -0500

    QuizGame
    The title screen should include the title of the game ("My Quiz Game" or similar) and a button to start the game.
    The game screen should include the question text, as well as 2 - 4 buttons for different answers.
    The ending screen should display how many questions the player got right/wrong, as well as a button for returning to the title screen.
    There are many ways you can handle the logic here, but the simplest might be to make different functions such as RightAnswer and WrongAnswer, that are handled by the appropriate buttons. Additionally, you will need a GameManager class to keep track of how many questions have been answered correctly. Be sure to reset the game's score when it is restarted!

---
+++ QuizGame/Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    public int correctScore = 0;
+    public int wrongScore = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ QuizGame/Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public int nextScene;
+    public void CorrectAnswer()
+    {
+
+        GameManager.instance.correctScore++;
+    }
+
+    public void WrongAnswer()
+    {
+        GameManager.instance.wrongScore++;
+    }
+
+    public void NextScene()
+    {
+        nextScene = SceneManager.GetActiveScene().buildIndex + 1;
+        SceneManager.LoadScene(nextScene);
+    }
+}
